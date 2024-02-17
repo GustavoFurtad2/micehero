d2b88b779d46ce3fb28b9bf6b93d8ae3159aead4
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-02-16 21:21:56 -0300

    Create songs.lua

---
+++ songs.lua
@@ -0,0 +1,22 @@
+local SONGLIST = {
+    [1] = {
+        name   = "Balaclava",
+        author = "Arctic Monkeys",
+        album  = "Favourite Worst Nightmare",
+    },
+
+    [2] = {
+        name   = "Old Yellow Bricks",
+        author = "Arctic Monkeys",
+        album  = "Favourite Worst Nightmare",
+        verses = {
+            {3000, "Old Yellow Bricks, Love's Risk"},
+        }
+    },
+
+    [3] = {
+        name   = "Pretty Visitors",
+        author = "Arctic Monkeys",
+        album  = "Humbug",
+    },
+}
