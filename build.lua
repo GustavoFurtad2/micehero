073669c8a8f08dc6fa08b490a711bc9d0265d051
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-02-16 21:21:08 -0300

    Create build.lua

---
+++ build.lua
@@ -0,0 +1,19 @@
+local files = {
+    "init",
+    "images",
+    "utils",
+    "songs",
+    "events",
+}
+
+Make = function()
+
+    local file = ""
+    make = io.open("build/make.lua", "w")
+    for k, f in next, files do
+        file = file .. io.open(f .. ".lua"):read("*a") .. "\n\n"
+    end
+    make:write(file)
+end
+
+Make()
